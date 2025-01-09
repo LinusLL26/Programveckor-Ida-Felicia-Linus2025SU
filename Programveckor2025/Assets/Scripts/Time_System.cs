731f31e60282701df347f8c8b236aabe0cba2d51
FV-SU24a <felicia.vasquez@elev.ga.lbs.se>
2025-01-09 11:01:48 +0100

    text

--- Programveckor2025/Assets/Scripts/Time_System.cs
+++ Programveckor2025/Assets/Scripts/Time_System.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Time_System : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI TextTimer;
+    [SerializeField] float RemainingTime;
+
     void Start()
     {
-        
+        RemainingTime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(RemainingTime / 60);
+        int Seconds = Mathf.FloorToInt(RemainingTime % 60);
+        TextTimer.text = string.Format("{0:00}:{1:00}", minutes, Seconds);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
