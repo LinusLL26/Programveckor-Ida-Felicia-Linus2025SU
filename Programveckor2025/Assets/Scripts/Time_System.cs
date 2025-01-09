065b01c4e74eaeaedaaf5e2b4b18ec8fb3c6d3d5
FV-SU24a <felicia.vasquez@elev.ga.lbs.se>
2025-01-09 11:12:05 +0100

    timer

--- Programveckor2025/Assets/Scripts/Time_System.cs
+++ Programveckor2025/Assets/Scripts/Time_System.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class Time_System : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI TextTimer;
-    [SerializeField] float RemainingTime;
+    [SerializeField] TextMeshProUGUI TextTimer; 
+    [SerializeField] float RemainingTime = 300f; 
 
-    void Start()
+    void Update()
     {
-        RemainingTime -= Time.deltaTime;
-        int minutes = Mathf.FloorToInt(RemainingTime / 60);
-        int Seconds = Mathf.FloorToInt(RemainingTime % 60);
-        TextTimer.text = string.Format("{0:00}:{1:00}", minutes, Seconds);
-    }
+        if (RemainingTime > 0)
+        {
+            RemainingTime -= Time.deltaTime;
 
+            int minutes = Mathf.FloorToInt(RemainingTime / 60);
+            int seconds = Mathf.FloorToInt(RemainingTime % 60);
+
+            TextTimer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else
+        {
+            TextTimer.text = "00:00"; 
+        }
+    }
 }
