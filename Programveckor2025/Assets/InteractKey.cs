e08c1faaa92d71925ac11473e627911427a28c81
LinusLL26 <linus.lagerdahllillieqvist@elev.ga.lbs.se>
2025-01-10 10:18:00 +0100

    added trigger to engage dialog

--- Programveckor2025/Assets/InteractKey.cs
+++ Programveckor2025/Assets/InteractKey.cs
@@ -5,28 +5,5 @@ using UnityEngine;
 
 public class InteractKey : MonoBehaviour
 {
-    public bool interacted;
-
-    [SerializeField]
-    KeyCode Interact;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        interacted = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(Interact))
-        {
-            interacted = true;
-            print("Player attempted to interact");
-        }
-        else
-        {
-            interacted = false;
-        }
-    }
+    public KeyCode InteractionKey;
 }
---
+++ Programveckor2025/Assets/Scripts/DialogCollection.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogCollection : MonoBehaviour
+{
+    //Dialog collection
+
+    //ID 1-3. Purely for testing purposes
+    public string dialogID1 = "Hello, this is my first line of dialog";
+    public string dialogID2 = "This is my second line of dialog";
+    public string dialogID3 = "This is my third and final line of dialog";
+}
