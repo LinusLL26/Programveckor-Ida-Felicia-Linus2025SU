101efaca08721f053caa670f1e2ae58b786d60a8
LinusLL26 <linus.lagerdahllillieqvist@elev.ga.lbs.se>
2025-01-14 10:51:09 +0100

    Still testing typewriter

--- Programveckor2025/Assets/TypingEffect.cs
+++ Programveckor2025/Assets/TypingEffect.cs
@@ -10,70 +10,34 @@ public class TypingEffect : MonoBehaviour
 {
     //all of this is copied from a youtube tutorial
 
-    private TMP_Text TextBox;
+    [Header("Test String")]
+    [SerializeField] private string testString;
 
-    //prototype text
-    [Header("test string")]
-    
-    [SerializeField] string testingText;
-   
-    private int currentlyVisibleCharactersIndex;
+    private TMP_Text TextBox;
+    private int currentlyVisibleCharacterIndex;
 
     private Coroutine TypeWriterCoroutine;
 
-    private WaitForSeconds BaseDelay;
-    private WaitForSeconds InterPunctuationDelay;
+    private WaitForSeconds _basicDelay;
+    private WaitForSeconds _interpuctuationdelay;
 
+    [Header("TypeWriter settings")]
     [SerializeField] private float charactersPerSecond = 20;
-    [SerializeField] private float extraInterPunctuationDelay = 0.5f;
-
-    
+    [SerializeField] private float interpuctuationDelay = 0.5f;
 
     private void Awake()
     {
         TextBox = GetComponent<TMP_Text>();
 
-        BaseDelay = new WaitForSeconds(1 / charactersPerSecond);
-        InterPunctuationDelay = new WaitForSeconds(extraInterPunctuationDelay);
-
+        _basicDelay = new WaitForSeconds(1 / charactersPerSecond);
+        _interpuctuationdelay = new WaitForSeconds(interpuctuationDelay);
     }
     private void Start()
     {
-        SetText(testingText);
+        
     }
     public void SetText(string text)
     {
-        if (TypeWriterCoroutine == null)
-        {
-            StopCoroutine(TypeWriterCoroutine);
-        }
 
-        TextBox.text = text;
-        TextBox.maxVisibleCharacters = 0;
-        currentlyVisibleCharactersIndex = 0;
-
-        TypeWriterCoroutine = StartCoroutine(routine: TypeWriter());
-    }
-    private IEnumerator TypeWriter()
-    {
-        TMP_TextInfo textInfo = TextBox.textInfo;
-
-        while (currentlyVisibleCharactersIndex < textInfo.characterCount + 1)
-        {
-            char character = textInfo.characterInfo[currentlyVisibleCharactersIndex].character;
-
-            TextBox.maxVisibleCharacters++;
-
-            if (character == '.' || character == '?' || character == '!' || character == ',' || character == ':' || character == ';' || character == '-')
-            {
-                yield return InterPunctuationDelay;
-            }
-            else
-            {
-                yield return BaseDelay;
-            }
-
-            currentlyVisibleCharactersIndex++;
-        }
     }
 }
